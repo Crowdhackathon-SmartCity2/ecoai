827ed32a28301de112b1736d0c8a6b60abed2df7
GiorgosDev <mayshmaz@gmail.com>
2018-07-01 11:07:25 +0300

    Lambda webhook added

---
+++ AWS-Lambda/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    FB_PAGE_TOKEN: '',
+    FB_VERIFY_TOKEN: '',
+    API_AI_CLIENT_ACCESS_TOKEN: '',
+    FB_APP_SECRET: '',
+    SERVER_URL: "",
+	S3_BUCKET_URL: "",
+	AWS_TOKEN: ""
+};
